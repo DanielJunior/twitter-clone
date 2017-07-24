7ad7b02bfd60d4d02699c0b057e0635ead42f3fd
DanielJunior <danieljunior@id.uff.br>
2017-07-24 18:25:11 -0300

    REFS #2 : Creating user profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  layout :layout_by_resource
 
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
+
+
 
   def index
 
@@ -14,6 +18,16 @@ class ApplicationController < ActionController::Base
     added_attrs = [:name,:description, :avatar, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
   end
 
 end
