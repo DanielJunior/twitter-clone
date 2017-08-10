7231d0e0aa7103c2897106522da6166bdc91d8a5
DanielJunior <danieljunior@id.uff.br>
2017-08-10 17:39:03 -0300

    REFS #4 : Fix tweet list

--- app/facades/tweet_facade.rb
+++ app/facades/tweet_facade.rb
@@ -5,10 +5,10 @@ class TweetFacade
   def initialize current_user, user_id
     @user = current_user
     @tweets = Tweet.where(user_id: user_id)
-    @tweet_owner = @tweets.first.user
+    @tweet_owner = @tweets.first.user unless @tweets.blank?
   end
 
   def title
-    @tweet_owner.id != @user.id ? "Tweets from #{@tweet_owner.name}" : "My tweets:"
+    !@tweet_owner.blank? && (@tweet_owner.id != @user.id) ? "Tweets from #{@tweet_owner.name}" : "My tweets:"
   end
 end
